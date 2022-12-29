2ac72aba7b0753aafc8603a6e05c2a901eb8ca33
Caleb Heuer <ch@raiguard.me>
2022-12-29 00:05:12 -0700

    Remove need for linked custom inputs and improve robustness

--- control.lua
+++ control.lua
@@ -1,6 +1,23 @@
 local bounding_box = require("__flib__/bounding-box")
 
-require("__core__/lualib/util")
+--- @class DragState
+--- @field entities table<uint, EntityData>
+--- @field item_name string
+--- @field last_tick uint
+--- @field num_entities integer
+--- @field player LuaPlayer
+
+--- @class EntityData
+--- @field count uint
+--- @field entity LuaEntity
+--- @field label uint64
+
+--- @class LastSelectedState
+--- @field cursor_count uint
+--- @field entity LuaEntity
+--- @field item_count uint
+--- @field item_name string
+--- @field tick uint
 
 --- @param player LuaPlayer
 --- @param item_name string
@@ -18,51 +35,32 @@ local function get_item_count(player, item_name)
 	return count --[[@as uint]]
 end
 
---- @class ActiveDragData
---- @field entities table<uint, EntityData>
---- @field initial_cursor_count uint
---- @field item_name string
---- @field item_count uint
---- @field last_tick uint
---- @field num_entities integer
---- @field original_group LuaPermissionGroup?
---- @field player LuaPlayer
-
---- @class EntityData
---- @field entity LuaEntity
---- @field label uint64
-
 script.on_init(function()
-	--- @type table<uint, ActiveDragData>
-	global.active_drags = {}
+	--- @type table<uint, DragState>
+	global.drag = {}
+	--- @type table<uint, LastSelectedState>
+	global.last_selected = {}
 end)
 
-script.on_event({ "edl-linked-fast-entity-transfer", "edl-linked-fast-entity-split" }, function(e)
+script.on_event(defines.events.on_selected_entity_changed, function(e)
 	local player = game.get_player(e.player_index)
 	if not player or not player.valid then
 		return
 	end
 
 	local selected = player.selected
-	-- Only do logic if the player doesn't start on an entity
-	if selected then
-		return
-	end
-
-	-- Only do logic if they are holding an item
 	local cursor_stack = player.cursor_stack
-	if not cursor_stack or not cursor_stack.valid_for_read then
+	if not selected or not cursor_stack or not cursor_stack.valid_for_read then
+		global.last_selected[e.player_index] = nil
 		return
 	end
 
-	global.active_drags[e.player_index] = {
-		entities = {},
-		initial_cursor_count = cursor_stack.count,
+	global.last_selected[e.player_index] = {
+		cursor_count = cursor_stack.count,
+		entity = selected,
 		item_count = get_item_count(player, cursor_stack.name),
 		item_name = cursor_stack.name,
-		last_tick = game.tick,
-		num_entities = 0,
-		player = player,
+		tick = game.tick,
 	}
 end)
 
@@ -71,13 +69,18 @@ script.on_event(defines.events.on_player_fast_transferred, function(e)
 		return
 	end
 
-	local data = global.active_drags[e.player_index]
-	if not data then
+	local entity = e.entity
+	if not entity.valid then
 		return
 	end
 
-	local player = data.player
-	if not player.valid then
+	local selected_state = global.last_selected[e.player_index]
+	if not selected_state or selected_state.tick ~= game.tick or selected_state.entity ~= entity then
+		return
+	end
+
+	local player = game.get_player(e.player_index)
+	if not player or not player.valid then
 		return
 	end
 
@@ -86,33 +89,41 @@ script.on_event(defines.events.on_player_fast_transferred, function(e)
 		return
 	end
 
-	local entity = e.entity
-	if not entity.valid then
-		return
+	local drag_state = global.drag[e.player_index]
+	if not drag_state then
+		--- @type DragState
+		drag_state = {
+			entities = {},
+			item_name = selected_state.item_name,
+			last_tick = game.tick,
+			num_entities = 0,
+			player = player,
+		}
+		global.drag[e.player_index] = drag_state
 	end
 
-	local new_count = get_item_count(player, data.item_name)
-	local inserted = data.item_count - new_count
+	local new_count = get_item_count(player, drag_state.item_name)
+	local inserted = selected_state.item_count - new_count --[[@as uint]]
 	if inserted == 0 then
 		return
 	end
 
 	-- Remove items from the destination and put them back into the player
-	entity.remove_item({ name = data.item_name, count = inserted })
+	entity.remove_item({ name = drag_state.item_name, count = inserted })
 	local new_cursor_count = cursor_stack.valid_for_read and cursor_stack.count or 0
-	local cursor_delta = data.initial_cursor_count - new_cursor_count
+	local cursor_delta = selected_state.cursor_count - new_cursor_count
 	if cursor_delta > 0 then
-		cursor_stack.set_stack({ name = data.item_name, count = data.initial_cursor_count })
+		cursor_stack.set_stack({ name = drag_state.item_name, count = selected_state.cursor_count })
 	end
 	new_count = new_count + cursor_delta
-	if new_count < data.item_count then
-		player.insert({ name = data.item_name, count = data.item_count - new_count })
+	if new_count < selected_state.item_count then
+		player.insert({ name = drag_state.item_name, count = selected_state.item_count - new_count })
 	end
 
-	data.last_tick = game.tick
+	drag_state.last_tick = game.tick
 
 	-- Add entity data if needed
-	local entity_data = data.entities[entity.unit_number]
+	local entity_data = drag_state.entities[entity.unit_number]
 	if not entity_data then
 		local label = rendering.draw_text({
 			color = { r = 1, g = 1, b = 1 },
@@ -123,12 +134,13 @@ script.on_event(defines.events.on_player_fast_transferred, function(e)
 			text = "",
 		})
 		entity_data = {
+			count = 0,
 			entity = entity,
 			label = label,
 		}
-		data.entities[entity.unit_number] = entity_data
+		drag_state.entities[entity.unit_number] = entity_data
 
-		data.num_entities = data.num_entities + 1
+		drag_state.num_entities = drag_state.num_entities + 1
 	end
 
 	-- Destroy flying text from transfer
@@ -141,8 +153,16 @@ script.on_event(defines.events.on_player_fast_transferred, function(e)
 	end
 
 	-- Update all item counts
-	for _, entity_data in pairs(data.entities) do
-		rendering.set_text(entity_data.label, math.floor(data.item_count / data.num_entities))
+	local base = math.floor(selected_state.item_count / drag_state.num_entities) --[[@as uint]]
+	local remainder = selected_state.item_count % drag_state.num_entities
+	for _, entity_data in pairs(drag_state.entities) do
+		local count = base
+		if remainder > 0 then
+			count = count + 1
+			remainder = remainder - 1
+		end
+		entity_data.count = count
+		rendering.set_text(entity_data.label, count)
 	end
 end)
 
@@ -152,46 +172,44 @@ local colors = {
 	red = { r = 1 },
 }
 
---- @param player_index uint
---- @param entity LuaEntity
---- @param color Color
---- @param text LocalisedString
-local function flying_text(player_index, entity, color, text)
-	entity.surface.create_entity({
-		name = "flying-text",
-		color = color,
-		position = entity.position,
-		render_player_index = player_index,
-		text = text,
-	})
-end
-
---- @param data ActiveDragData
+--- @param data DragState
 local function finish_drag(data)
-	-- TODO: Insert all items as best as possible
-	local to_insert = math.floor(data.item_count / data.num_entities) --[[@as uint]]
+	if not data.player.valid then
+		return
+	end
 	local item_name = data.item_name
 	local item_localised_name = game.item_prototypes[item_name].localised_name
 	for _, entity_data in pairs(data.entities) do
 		rendering.destroy(entity_data.label)
-		local inserted = entity_data.entity.insert({ name = item_name, count = to_insert })
+		if entity_data.count == 0 then
+			goto continue
+		end
+		local inserted = entity_data.entity.insert({ name = item_name, count = entity_data.count })
 		local color = colors.white
 		if inserted == 0 then
 			color = colors.red
-		elseif inserted < to_insert then
+		elseif inserted < entity_data.count then
 			color = colors.yellow
 		end
-		flying_text(data.player.index, entity_data.entity, color, { "", "-", inserted, " ", item_localised_name })
+		local entity = entity_data.entity
+		entity.surface.create_entity({
+			name = "flying-text",
+			color = color,
+			position = entity.position,
+			render_player_index = data.player.index,
+			text = { "", "-", inserted, " ", item_localised_name },
+		})
 		if inserted > 0 then
 			data.player.remove_item({ name = item_name, count = inserted })
 		end
+		::continue::
 	end
 end
 
 script.on_event(defines.events.on_tick, function()
-	for player_index, drag_data in pairs(global.active_drags) do
+	for player_index, drag_data in pairs(global.drag) do
 		if drag_data.last_tick + 60 <= game.tick then
-			global.active_drags[player_index] = nil
+			global.drag[player_index] = nil
 			finish_drag(drag_data)
 		end
 	end
