d81dd638e683d9d6b1bf36037dc31e4417ce99be
Caleb Heuer <ch@raiguard.me>
2022-12-28 23:10:58 -0700

    Working prorotype with janky undoing of insertion

--- control.lua
+++ control.lua
@@ -1,8 +1,28 @@
--- FIXME: This doesn't actually detect control+drag, it just detects control+click at the start
+local bounding_box = require("__flib__/bounding-box")
+
+require("__core__/lualib/util")
+
+--- @param player LuaPlayer
+--- @param item_name string
+--- @return uint
+local function get_item_count(player, item_name)
+	local count = 0
+	local cursor_stack = player.cursor_stack
+	if cursor_stack and cursor_stack.valid_for_read and cursor_stack.name == item_name then
+		count = count + cursor_stack.count
+	end
+	local main_inventory = player.get_main_inventory()
+	if main_inventory and main_inventory.valid then
+		count = count + main_inventory.get_item_count(item_name)
+	end
+	return count --[[@as uint]]
+end
 
 --- @class ActiveDragData
 --- @field entities table<uint, EntityData>
---- @field item ItemStackIdentification
+--- @field initial_cursor_count uint
+--- @field item_name string
+--- @field item_count uint
 --- @field last_tick uint
 --- @field num_entities integer
 --- @field original_group LuaPermissionGroup?
@@ -15,13 +35,6 @@
 script.on_init(function()
 	--- @type table<uint, ActiveDragData>
 	global.active_drags = {}
-
-	local group = game.permissions.create_group("Evenly Distributing")
-	if not group then
-		error("Unable to create permission group")
-	end
-	group.set_allows_action(defines.input_action.fast_entity_transfer, false)
-	group.set_allows_action(defines.input_action.fast_entity_split, false)
 end)
 
 script.on_event({ "edl-linked-fast-entity-transfer", "edl-linked-fast-entity-split" }, function(e)
@@ -42,85 +55,94 @@ script.on_event({ "edl-linked-fast-entity-transfer", "edl-linked-fast-entity-spl
 		return
 	end
 
-	local count = cursor_stack.count
-	local main_inventory = player.get_main_inventory()
-	if main_inventory and main_inventory.valid then
-		count = count + main_inventory.get_item_count(cursor_stack.name)
-	end
-
-	if e.input_name == "edl-linked-fast-entity-split" then
-		count = math.ceil(count / 2) --[[@as uint]]
-	end
-
-	local group = player.permission_group
-	if group then
-		group.remove_player(player)
-	end
-	local ed_group = game.permissions.get_group("Evenly Distributing")
-	if not ed_group then
-		error("Even Distribution permission group was not created")
-	end
-	ed_group.add_player(player)
-
 	global.active_drags[e.player_index] = {
 		entities = {},
-		item = { name = cursor_stack.name, count = count },
+		initial_cursor_count = cursor_stack.count,
+		item_count = get_item_count(player, cursor_stack.name),
+		item_name = cursor_stack.name,
 		last_tick = game.tick,
 		num_entities = 0,
-		original_group = group,
 		player = player,
 	}
 end)
 
-script.on_event(defines.events.on_selected_entity_changed, function(e)
-	local drag_data = global.active_drags[e.player_index]
-	if not drag_data then
+script.on_event(defines.events.on_player_fast_transferred, function(e)
+	if not e.from_player then
 		return
 	end
 
-	local player = drag_data.player
+	local data = global.active_drags[e.player_index]
+	if not data then
+		return
+	end
+
+	local player = data.player
 	if not player.valid then
-		return -- Will be cleaned up in on_tick
-	end
-
-	local entity = player.selected
-	if not entity then
 		return
 	end
 
-	-- Transport belts will incorrectly return true in can_insert
-	if entity.type == "transport-belt" or entity.type == "underground-belt" or entity.type == "splitter" then
+	local cursor_stack = player.cursor_stack
+	if not cursor_stack then
 		return
 	end
 
-	if not entity.can_insert(drag_data.item) then
+	local entity = e.entity
+	if not entity.valid then
 		return
 	end
 
-	drag_data.last_tick = game.tick
-
-	local entity_data = drag_data.entities[entity.unit_number]
-	if entity_data then
+	local new_count = get_item_count(player, data.item_name)
+	local inserted = data.item_count - new_count
+	if inserted == 0 then
 		return
 	end
 
-	drag_data.entities[entity.unit_number] = {
-		entity = entity,
-		label = rendering.draw_text({
+	-- Remove items from the destination and put them back into the player
+	entity.remove_item({ name = data.item_name, count = inserted })
+	local new_cursor_count = cursor_stack.valid_for_read and cursor_stack.count or 0
+	local cursor_delta = data.initial_cursor_count - new_cursor_count
+	if cursor_delta > 0 then
+		cursor_stack.set_stack({ name = data.item_name, count = data.initial_cursor_count })
+	end
+	new_count = new_count + cursor_delta
+	if new_count < data.item_count then
+		player.insert({ name = data.item_name, count = data.item_count - new_count })
+	end
+
+	data.last_tick = game.tick
+
+	-- Add entity data if needed
+	local entity_data = data.entities[entity.unit_number]
+	if not entity_data then
+		local label = rendering.draw_text({
 			color = { r = 1, g = 1, b = 1 },
 			only_in_alt_mode = true,
 			players = { e.player_index },
 			surface = entity.surface,
 			target = entity,
 			text = "",
-		}),
-	}
+		})
+		entity_data = {
+			entity = entity,
+			label = label,
+		}
+		data.entities[entity.unit_number] = entity_data
 
-	local num_entities = drag_data.num_entities + 1
-	drag_data.num_entities = num_entities
+		data.num_entities = data.num_entities + 1
+	end
 
-	for _, entity_data in pairs(drag_data.entities) do
-		rendering.set_text(entity_data.label, math.floor(drag_data.item.count / num_entities))
+	-- Destroy flying text from transfer
+	local flying_text = entity.surface.find_entities_filtered({
+		name = "flying-text",
+		area = bounding_box.recenter_on(entity.prototype.selection_box, entity.position),
+	})[1]
+	if flying_text then
+		flying_text.destroy()
+	end
+
+	-- Update all item counts
+	for _, entity_data in pairs(data.entities) do
+		rendering.set_text(entity_data.label, math.floor(data.item_count / data.num_entities))
 	end
 end)
 
@@ -146,20 +168,9 @@ end
 
 --- @param data ActiveDragData
 local function finish_drag(data)
-	-- Reset permission group
-	local player = data.player
-	local group = player.permission_group
-	if group then
-		group.remove_player(player)
-	end
-	local original_group = data.original_group
-	if original_group and original_group.valid then
-		original_group.add_player(player)
-	end
-
-	-- Insert items
-	local to_insert = math.floor(data.item.count / data.num_entities) --[[@as uint]]
-	local item_name = data.item.name
+	-- TODO: Insert all items as best as possible
+	local to_insert = math.floor(data.item_count / data.num_entities) --[[@as uint]]
+	local item_name = data.item_name
 	local item_localised_name = game.item_prototypes[item_name].localised_name
 	for _, entity_data in pairs(data.entities) do
 		rendering.destroy(entity_data.label)
